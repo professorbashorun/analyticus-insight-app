d770627d6be8fb99c755615870b9d83414bfd66b
System Administrator <root@Professorbashorun.local>
2017-11-19 14:59:01 -0800

    new changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-217-12-69.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/key1026.pem',
-      ref: 'master',
-      repo: 'https://github.com/professorbashorun/analyticus-insight-app',
+      ref: 'origin/master',
+      repo: 'git@github.com:professorbashorun/analyticus-insight-app.git',
       path: '/analyticus-insight',
       'post-deploy': 'sudo npm install && sudo pm2 startOrRestart ecosystem.config.js'
     }
