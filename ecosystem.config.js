be737a6c310ffe6341a79e5b1229c0687b73c209
System Administrator <root@Professorbashorun.local>
2017-11-19 14:49:58 -0800

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'analyticus-insight',
+    script: './bin/www'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-217-12-69.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/key1026.pem',
+      ref: 'master',
+      repo: 'https://github.com/professorbashorun/analyticus-insight-app',
+      path: '/analyticus-insight',
+      'post-deploy': 'sudo npm install && sudo pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
