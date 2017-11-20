5267e38f18c0f34b5bb647346e382ccc82ee4d07
System Administrator <root@Professorbashorun.local>
2017-11-20 08:59:53 -0800

    done

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   apps: [{
     name: 'analyticus-insight',
@@ -10,6 +11,27 @@ module.exports = {
       key: '~/.ssh/key1026.pem',
       ref: 'master',
       repo: 'https://github.com/professorbashorun/analyticus-insight-app',
+      path: '/analyticus-insight',
+      'post-deploy': 'sudo git pull https://github.com/professorbashorun/analyticus-insight-app && sudo npm install && sudo pm2 startOrRestart /analyticus-insight/ecosystem.config.js'
+    }
+  }
+}
+*/
+
+
+
+module.exports = {
+  apps: [{
+    name: 'analyticus-insight',
+    script: '../bin/www'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-217-12-69.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/key1026.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:professorbashorun/analyticus-insight-app.git',
       path: '/analyticus-insight',
       'post-deploy': 'sudo npm install && sudo pm2 startOrRestart /analyticus-insight/ecosystem.config.js'
     }
