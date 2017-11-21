1079cb3f39aa004a5a88e4377ff15259cc5f8fe3
Professor Bashorun <macbookair@Professorbashorun.local>
2017-11-20 23:17:05 -0800

    here

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-217-12-69.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/key1026.pem',
-      ref: 'origin/master',
+      ref: 'master',
       repo: 'git@github.com:professorbashorun/analyticus-insight-app.git',
       path: '/analyticus-insight',
       'post-deploy': 'sudo git pull https://github.com/professorbashorun/analyticus-insight-app && sudo npm install && sudo pm2 startOrRestart ./ecosystem.config.js'
