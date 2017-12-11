e189188f3a24852ea8366f3fe55af8f765a94079
Your Name <professorbashorun@gmail.com>
2017-12-11 05:24:31 +0100

    new version

---
+++ connect.sh
@@ -0,0 +1,2 @@
+chmod 400 key1026.pem;
+ssh -i "key1026.pem" ubuntu@ec2-18-217-12-69.us-east-2.compute.amazonaws.com;
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,3 @@
-/*
-module.exports = {
-  apps: [{
-    name: 'analyticus-insight',
-    script: '../bin/www'
-  }],
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: 'ec2-18-217-12-69.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/key1026.pem',
-      ref: 'master',
-      repo: 'https://github.com/professorbashorun/analyticus-insight-app',
-      path: '/analyticus-insight',
-      'post-deploy': 'sudo git pull https://github.com/professorbashorun/analyticus-insight-app && sudo npm install && sudo pm2 startOrRestart /analyticus-insight/ecosystem.config.js'
-    }
-  }
-}
-*/
-
-
-
 module.exports = {
   apps: [{
     name: 'analyticus-insight',
@@ -32,8 +10,9 @@ module.exports = {
       key: '~/.ssh/key1026.pem',
       ref: 'master',
       repo: 'git@github.com:professorbashorun/analyticus-insight-app.git',
-      path: '/analyticus-insight',
+      path: './',
       'post-deploy': 'sudo git pull https://github.com/professorbashorun/analyticus-insight-app && sudo npm install && sudo pm2 startOrRestart /analyticus-insight/ecosystem.config.js'
     }
   }
 }
+
